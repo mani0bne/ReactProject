cdf297a43494b9cb9d609a5a1e774e785114ffbb
mani0bne <manihasnetha@gmail.com>
2024-10-15 12:53:02 +0530

    Added Img Links

--- src/App.jsx
+++ src/App.jsx
@@ -34,6 +34,8 @@
 //     )
 //   }
 // }
+//export default app;
+
 // import './App.css'
 // function App(){ 
 //   return(
@@ -60,36 +62,52 @@
 // }
 // export default App;
 
-import {BrowserRouter, Route, Routes, Link} from 'react-router-dom'
-import Login from "./Login"
-import Signup from "./Signup"
-import About from "./About"
+// import {BrowserRouter, Route, Routes, Link} from 'react-router-dom'
+// import Login from "./Login"
+// import Signup from "./Signup"
+// import About from "./About"
 
-function Header(){
-  return(
-    <ul>
-      <li><Link to = "/">Login</Link></li>
-      <li><Link to= "/Signup">Signup</Link></li>
-      <li><Link to = "/About">About</Link></li>
-    </ul>
-  )
-}
+// function Header(){
+//   return(
+//     <ul>
+//       <li><Link to = "/">Login</Link></li>
+//       <li><Link to= "/Signup">Signup</Link></li>
+//       <li><Link to = "/About">About</Link></li>
+//     </ul>
+//   )
+// }
+
+
+// function App(){
+//   return(
+//     <>
+    
+//     <BrowserRouter>
+//     <Header/>
+//     <Routes>
+//     <Route path="/" element={<Login/>}/>
+//     <Route path="/Signup" element={<Signup/>}/>
+//     <Route path = "/About" element={<About/>}/>
+//     <Route/>
+//     <Route/>
+//     </Routes>
+//     </BrowserRouter>
+//     </>
+//   )
+// }
+
+// export default App;
 
 function App(){
   return(
-    <>
-    
-    <BrowserRouter>
-    <Header/>
-    <Routes>
-    <Route path="/" element={<Login/>}/>
-    <Route path="/Signup" element={<Signup/>}/>
-    <Route path = "/About" element={<About/>}/>
-    <Route/>
-    <Route/>
-    </Routes>
-    </BrowserRouter>
-    </>
+    <div>
+    <img src={"img.jpg"} height={200} width={200}/>
+    <br/>
+    <br/>
+    <img src={"src/assets/img.jpg"} height={200} width={200}/>
+    <br />
+    <img src="https://yt3.ggpht.com/ytc/AKedOLSUTmuXysocNOrXD86JiszqCXePK12O9GTWfr-Z=s900-c-k-c0x00ffffff-no-rj" alt="cduimg" height={200} width={200} />
+    </div>
   )
 }
 
